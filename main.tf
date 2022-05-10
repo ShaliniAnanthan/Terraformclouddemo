cbeaa6055183138cb5a463bfab33d9bc74e60d0a
ShaliniAnanthan <shalini.ananthan05@gmail.com>
2022-05-10 11:10:07 +0530

    file added

---
+++ main.tf
@@ -0,0 +1,33 @@
+ provider "google" {
+     project     = "gcp-terraform-348507"
+     region      = "asia-east1"
+     zone        = "asia-east1-c"
+     credentials = file("gcp-accounts.json")
+}
+
+resource "google_compute_instance" "default" {
+  name         = "server-${count.index}"
+  count = 3
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+resource "google_storage_bucket" "default" {
+  name = "terraformbucket02"
+  project = "gcp-terraform-348507"
+  storage_class = "REGIONAL"
+  location = "us-east1"
+}
